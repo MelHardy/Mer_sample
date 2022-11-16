08463723d35f201a8c66231e4cb6b343b06f3eb7
MelHardy <113648082+MelHardy@users.noreply.github.com>
2022-11-16 15:31:17 -0800

    Add files via upload

---
+++ twobinary_onette_endpoints_v1.R
@@ -0,0 +1,77 @@
+##Test with 6 participants and 2 binary endpoints (mortality at 30 days and clinical/micro response) 
+##and 2 columns with time to event endpoints (death or clinical/micro response)
+install.packages("tidyverse")
+install.packages("summarytools")
+library(tidyverse)
+library(summarytools)
+
+##Read in test data file with columns id, intervention (control 1,treatment 2), mortality (binary,1,0), 
+##clinical micro response (binary, 1,0). This dataset has non sequential id's
+data <-read_csv("wrtest_unseq.csv")
+
+##Set up vector for first endpoint
+wrtest <- c()
+
+##Create a loop for the first binary endpoint and calculate wins (1), losses (-1) and draws (0)
+for (i in data$id[which(data$intervention == 1)]){
+  for (j in data$id[which(data$intervention == 2)]){ 
+    if(data$mortality_30d[data$id == i]<data$mortality_30d[data$id ==j]){
+      wrtest <- c(wrtest,-1)}
+    else if(data$mortality_30d[data$id == i]>data$mortality_30d[data$id == j]){
+      wrtest <- c(wrtest,1)}
+    else if(data$mortality_30d[data$id == i]==data$mortality_30d[data$id == j]){
+      wrtest <- c(wrtest,0)}
+  }
+  wrtest  
+}
+
+mortality <-data.frame(wrtest)
+
+mortality$wrtest <- factor(mortality$wrtest, levels=c(-1,0,1), labels=c("loss", "draw", "win"))
+freq(mortality$wrtest)
+
+##Set up a second vector for a second endpoint
+wrtest2 <- c()
+
+##Create a loop for the second binary endpoint that calculates the wins (2), losses (-2)
+##and draws (0) for the second endpoint clinical/microbilogical response
+for (i in data$id[which(data$intervention == 1)]){
+  for (j in data$id[which(data$intervention == 2)]){ 
+    if(data$mortality_30d[data$id == i]==data$mortality_30d[data$id == j]){{
+      if(data$cm_response[data$id == i]<data$cm_response[data$id == j]){
+        wrtest2 <- c(wrtest2,2)}
+      else if(data$cm_response[data$id == i]>data$cm_response[data$id == j]){
+        wrtest2 <- c(wrtest2,-2)}
+      else if(data$cm_response[data$id == i]==data$cm_response[data$id == j])
+        wrtest2 <- c(wrtest2,0)}}
+  }
+  wrtest2  
+}
+
+cm_response <-data.frame(wrtest2)
+
+cm_response$wrtest2 <- factor(cm_response$wrtest2, levels=c(-2,0,2), labels=c("loss", "draw", "win"))
+freq(cm_response$wrtest2)
+
+##Set up a third vector for a third endpoint which is tte
+wrtest3 <- c()
+
+##Create a loop that calculate the wins (3), losses (-3) and draws (0) for the third TTE endpoint 
+##microbiological relapse
+for (i in data$id[which(data$intervention == 1)]){
+  for (j in data$id[which(data$intervention == 2)]){ 
+    if((data$mortality_30d[data$id == i]==data$mortality_30d[data$id == j]) & (data$cm_response[data$id == i]==data$cm_response[data$id == j])){{
+      if(data$days_microrelapse[data$id == i]>data$days_microrelapse[data$id == j]){
+        wrtest3 <- c(wrtest3,3)}
+      else if(data$days_microrelapse[data$id == i]<data$days_microrelapse[data$id == j]){
+        wrtest3 <- c(wrtest3,-3)}
+      else if(data$days_microrelapse[data$id == i]==data$days_microrelapse[data$id == j])
+        wrtest3 <- c(wrtest3,0)}}
+  }
+  wrtest3  
+}
+
+tt_microrelapse <-data.frame(wrtest3)
+
+tt_microrelapse$wrtest3 <- factor(tt_microrelapse$wrtest3, levels=c(-3,0,3), labels=c("loss", "draw", "win"))
+freq(tt_microrelapse$wrtest3)
